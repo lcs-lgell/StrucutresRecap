72b91962c7e65f35aa2043b4bc4832cac997f65a
Leon Gell <lgell@lcs.on.ca>
2023-01-25 11:18:21 -0500

    Initial Commit

---
+++ StrucutresRecap/StrucutresRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrucutresRecapApp.swift
+//  StrucutresRecap
+//
+//  Created by Leon Gell on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct StrucutresRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
